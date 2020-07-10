42a08491f4ceb1ede3e8602a8fae3fe7e041fd59
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-07-10 16:09:58 +0530

    change

--- graphql mongo/src/resolvers/Subscription.js
+++ graphql mongo/src/resolvers/Subscription.js
@@ -4,27 +4,27 @@ import { Comment } from "../models/Comment";
 import getUserId from "../utils/getUserId";
 
 const Subscription = {
-    comment: {
-        subscribe(parent, { postId }, { db, pubsub,request }, info){
-            const post = Post.findById(postId)
-            // const userId = getUserId(request)
+	comment: {
+		subscribe(parent, { postId }, { db, pubsub, request }, info) {
+			const post = Post.findById(postId);
+			// const userId = getUserId(request)
 
-            // if(post.author!=userId){
-            //     throw new Error('This is not your post, you cant subscribe to the comments')
-            // }
+			// if(post.author!=userId){
+			//     throw new Error('This is not your post, you cant subscribe to the comments')
+			// }
 
-            if (!post) {
-                throw new Error('Post not found')
-            }
+			if (!post) {
+				throw new Error("Post not found");
+			}
 
-            return pubsub.asyncIterator(`comment ${postId}`)
-        }
-    },
-    post: {
-        subscribe(parent, args, { pubsub }, info) {
-            return pubsub.asyncIterator('post')
-        }
-    }
-}
+			return pubsub.asyncIterator(`comment ${postId}`);
+		},
+	},
+	post: {
+		subscribe(parent, args, { pubsub }, info) {
+			return pubsub.asyncIterator("post");
+		},
+	},
+};
 
-export { Subscription as default }
+export { Subscription as default };
